22206f1bf09e35140db7dd94a7adcf5cc2c9fa2c
Rabbitg <fi8608@naver.com>
2020-04-14 14:29:40 +0900

    Tutorial 4

--- app/src/main/java/com/hour/uigithub/MainActivity.kt
+++ app/src/main/java/com/hour/uigithub/MainActivity.kt
@@ -1,31 +1,90 @@
 package com.hour.uigithub
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
+import com.hour.uigithub.util.NotificationUtil
+import com.hour.uigithub.util.PrefUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_timer.*
 import kotlinx.android.synthetic.main.goal_dialog.view.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+
+    companion object{
+        val TAG = "MainActivity"
+        fun setAlarm(context: Context, nowSeconds: Long, secondsRemaining: Long): Long{
+            val wakeUpTime = (nowSeconds + secondsRemaining) * 1000
+            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            val intent = Intent(context, TimerExpiredReceiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, wakeUpTime, pendingIntent)
+            PrefUtil.setAlarmSetTime(nowSeconds, context)
+            return wakeUpTime
+        }
+
+        fun removeAlarm(context: Context){
+            val intent = Intent(context, TimerExpiredReceiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            alarmManager.cancel(pendingIntent)
+            PrefUtil.setAlarmSetTime(0, context)
+        }
+
+        val nowSeconds: Long
+        get() = Calendar.getInstance().timeInMillis/1000
+
+    }
+    // 카운트 다운 타이머
+    enum class TimerState{
+        Stopped, Paused, Running
+    }
+
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds : Long = 0L
+    private var timerState = TimerState.Stopped
+    private var secondsRemaining = 0L
 
     // Firebase database 접근하기 위한 것
     lateinit var mDatabase: DatabaseReference
 
     private val database by lazy { FirebaseDatabase.getInstance() }
 
-    companion object{
-        val TAG = "MainActivity"
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setIcon(R.drawable.ic_timer)
+        supportActionBar?.title = "      Timer"
+        // 카운트다운
+        fab_start.setOnClickListener { v ->
+            startTimer()
+            timerState = TimerState.Running
+            updateButtons()
+        }
+
+        fab_pause.setOnClickListener { v->
+            timer.cancel()
+            timerState = TimerState.Paused
+            updateButtons()
+        }
+
+        fab_stop.setOnClickListener {
+            timer.cancel()
+            onTimerFinished()
+        }
+
 
         // Firebase Instance 얻어오기 위한 것
         mDatabase = FirebaseDatabase.getInstance().reference
@@ -43,11 +102,14 @@ class MainActivity : AppCompatActivity() {
                 dataSnapshot.children.forEach {
                     //"it" is the snapshot
                     val key: String = it.key.toString()
-                    val value : Any? = dataSnapshot.child(key).child("title").value.toString()
-                    title_mainTextView.text = value.toString()
+                    val title : Any? = dataSnapshot.child(key).child("title").value.toString()
+                    title_mainTextView.text = title.toString()
+
+                    val description : Any? = dataSnapshot.child(key).child("description").value.toString()
+                    description_mainTextView.text = description.toString()
                 }
 
-              // val value = dataSnapshot.child("title").value
+                // val value = dataSnapshot.child("title").value
 
 
 
@@ -61,6 +123,130 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        initTimer()
+
+        removeAlarm(this)
+        NotificationUtil.hideTimerNotification(this)
+
+        //TODO: remove background timer, hide notification (백그라운드 타이머 제거하고 노티피케이션 숨긴다)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+
+        if (timerState == TimerState.Running){
+            timer.cancel()
+            val wakeUpTime = setAlarm(this, nowSeconds, secondsRemaining)
+            NotificationUtil.showTimerRunning(this, wakeUpTime)
+        }
+        else if (timerState == TimerState.Paused){
+            NotificationUtil.showTimerPaused(this)
+        }
+
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, this)
+        PrefUtil.setSecondsRemaining(secondsRemaining, this)
+        PrefUtil.setTimerState(timerState, this)
+    }
+
+    private fun initTimer(){
+        timerState = PrefUtil.getTimerState(this)
+
+        //we don't want to change the length of the timer which is already running
+        //if the length was changed in settings while it was backgrounded
+        if (timerState == TimerState.Stopped)
+            setNewTimerLength()
+        else
+            setPreviousTimerLength()
+
+        secondsRemaining = if (timerState == TimerState.Running || timerState == TimerState.Paused)
+            PrefUtil.getSecondsRemaining(this)
+        else
+            timerLengthSeconds
+
+        val alarmSetTime = PrefUtil.getAlarmSetTime(this)
+        if (alarmSetTime > 0)
+            secondsRemaining -= nowSeconds - alarmSetTime
+
+        if (secondsRemaining <= 0)
+            onTimerFinished()
+        else if (timerState == TimerState.Running)
+            startTimer()
+
+        updateButtons()
+        updateCountdownUI()
+    }
+
+    private fun onTimerFinished(){
+        timerState = TimerState.Stopped
+
+        //set the length of the timer to be the one set in SettingsActivity
+        //if the length was changed when the timer was running
+        setNewTimerLength()
+
+        progress_countdown.progress = 0
+
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, this)
+        secondsRemaining = timerLengthSeconds
+
+        updateButtons()
+        updateCountdownUI()
+    }
+
+    private fun startTimer(){
+        timerState = TimerState.Running
+
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
+            override fun onFinish() = onTimerFinished()
+
+            override fun onTick(millisUntilFinished: Long) {
+                secondsRemaining = millisUntilFinished / 1000
+                updateCountdownUI()
+            }
+        }.start()
+    }
+
+    private fun setNewTimerLength(){
+        val lengthInMinutes = PrefUtil.getTimerLength(this)
+        timerLengthSeconds = (lengthInMinutes * 60L)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun setPreviousTimerLength(){
+        timerLengthSeconds = PrefUtil.getPreviousTimerLengthSeconds(this)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun updateCountdownUI(){
+        val minutesUntilFinished = secondsRemaining / 60
+        val secondsInMinuteUntilFinished = secondsRemaining - minutesUntilFinished * 60
+        val secondsStr = secondsInMinuteUntilFinished.toString()
+        textView_countdown.text = "$minutesUntilFinished:${if (secondsStr.length == 2) secondsStr else "0" + secondsStr}"
+        progress_countdown.progress = (timerLengthSeconds - secondsRemaining).toInt()
+    }
+
+    private fun updateButtons(){
+        when (timerState) {
+            TimerState.Running ->{
+                fab_start.isEnabled = false
+                fab_pause.isEnabled = true
+                fab_stop.isEnabled = true
+            }
+            TimerState.Stopped -> {
+                fab_start.isEnabled = true
+                fab_pause.isEnabled = false
+                fab_stop.isEnabled = false
+            }
+            TimerState.Paused -> {
+                fab_start.isEnabled = true
+                fab_pause.isEnabled = false
+                fab_stop.isEnabled = true
+            }
+        }
+    }
 
 
     private fun addItemDialog() {
@@ -80,9 +266,9 @@ class MainActivity : AppCompatActivity() {
         mDialogView.dialogSubmitBtn.setOnClickListener {
 
             if (mDialogView.dialogTitleEt.text.toString().isEmpty() || mDialogView.dialogDiscriptionEt.text.toString().isEmpty())
-                {
+            {
                 Toast.makeText(this, "모두 입력 해주셔야 합니다 !!", Toast.LENGTH_SHORT).show()
-                }
+            }
             else
             {
                 // Firebase 의 Goal 참조에서 객체를 저장하기 위한 새로운 키를 생성하고 참조를 newRef 에 저장
@@ -102,6 +288,7 @@ class MainActivity : AppCompatActivity() {
                 goal.description = description_mainTextView.text.toString()
                 goal.writeTime = ServerValue.TIMESTAMP
                 newRef.setValue(goal)
+
                 // 다이얼로그 사라지게 하는 것
                 mAlertDialog.dismiss()
             }
---
+++ app/src/main/java/com/hour/uigithub/TimerExpiredReceiver.kt
@@ -0,0 +1,18 @@
+package com.hour.uigithub
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.hour.uigithub.util.NotificationUtil
+import com.hour.uigithub.util.PrefUtil
+
+class TimerExpiredReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        //TODO:show notification
+        NotificationUtil.showTimerExpired(context)
+
+        PrefUtil.setTimerState(MainActivity.TimerState.Stopped, context)
+        PrefUtil.setAlarmSetTime(0, context)
+    }
+}
